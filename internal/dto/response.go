224d24359ca7614e414d7864ceae12a39740bc06
v4lomyr <bintangdwitama160501@gmail.com>
2023-07-23 11:46:25 +0700

    feat(response): uniform response structure

---
+++ internal/dto/response.go
@@ -0,0 +1,18 @@
+package dto
+
+type ErrorResponse struct {
+	Message string      `json:"message"`
+	Code    string      `json:"code"`
+	Data    interface{} `json:"data"`
+}
+
+type SuccessResponse struct {
+	Message string      `json:"message"`
+	Code    string      `json:"code"`
+	Data    interface{} `json:"data"`
+}
+
+type ValidationErrorResponse struct {
+	Field   string `json:"field,omitempty"`
+	Message string `json:"message"`
+}
